29b6a93dfe1bed5773ace9caa4ea365fb6d343c9
ia <paepulumbarit@gmail.com>
2025-03-29 12:03:30 -0700

    Remove hardcoded meme data

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,17 @@
+import Meme from "@/types/meme"
+import React, { useState } from "react"
+
 export default function Main() {
-  /**
-   * Challenge: move the hardcoded meme info into React
-   * state. Use an object with `topText`, `bottomText`,
-   * and `imageUrl` properties, and set the initial values to
-   * the ones hardcoded below.
-   */
+  const [meme, setMeme] = useState<Meme>({
+    topText: 'One does not simply',
+    bottomText: 'Walk into Mordor',
+    imageUrl: 'http://i.imgflip.com/1bij.jpg'
+  })
+
+  function handleOnChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value, name } = event.currentTarget
+    setMeme(prevMeme => ({ ...prevMeme, [name]: value }))
+  }
 
   return (
     <main>
@@ -12,24 +19,26 @@ export default function Main() {
         <label>Top Text
           <input
             type="text"
-            placeholder="One does not simply"
+            placeholder={meme.topText}
             name="topText"
+            onChange={handleOnChange}
           />
         </label>
 
         <label>Bottom Text
           <input
             type="text"
-            placeholder="Walk into Mordor"
+            placeholder={meme.bottomText}
             name="bottomText"
+            onChange={handleOnChange}
           />
         </label>
-        <button>Get a new meme image 🖼</button>
+        <button>Get a new meme image </button>
       </div>
       <div className="meme">
-        <img src="http://i.imgflip.com/1bij.jpg" />
-        <span className="top">One does not simply</span>
-        <span className="bottom">Walk into Mordor</span>
+        <img src={meme.imageUrl}/>
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
       </div>
     </main>
   )
